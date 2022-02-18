134e316037f9524fa64ba345d411668a6d140451
Baptiste-u <Baptistedelorme10@gmail.com>
2022-02-18 15:25:38 +0100

    TEST dossier tinquiete

---
+++ Tuto_2 (D)/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:gsb_application/screens/home.dart';
+import 'screens/auth.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      // home: Home(),
+      home: AuthScreen(),
+    );
+  }
+}
