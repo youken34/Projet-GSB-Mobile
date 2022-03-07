e33f7af386527fc20475fc4b2e66aa9990d0d09b
Youss69 <youssoufkonta69@outlook.fr>
2022-03-07 22:25:24 +0100

    Envoie du test application mobile

--- Tuto_2 (D)/lib/screens/auth.dart
+++ Tuto_2 (D)/lib/screens/auth.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_final_fields
+
 import 'package:flutter/material.dart';
 
 class AuthScreen extends StatefulWidget {
@@ -8,12 +10,75 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+
+// ignore: unused_field
+bool _secureText = true;
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: const Scaffold(
-          body: Center(
-        child: Text('test'),
+      child:  Scaffold(
+        appBar: AppBar(
+          title: const Text('Projet GSB'),
+        ),
+          body: Column(
+            children: const [
+              SizedBox(
+                  height: 10,
+              ),
+         TextField(
+          decoration: InputDecoration(
+            hintText: "Entrer votre identifiant", // hintext = placeholder
+            labelText: "Identifiant",  // Nom de  l'input (juste au dessus)
+            labelStyle: TextStyle( // labelStyle correspond à la balise <style> en css
+                fontSize: 24,
+                color: Colors.blue
+            ), 
+            //border: OutlineInputBorder() // bordure autout d'un input
+            //border: InputBorder.none
+            border: UnderlineInputBorder(),
+            fillColor: Colors.black12,
+            filled: true, // syntaxe obligatoire, par defaut le remplissage filled = false
+            
+          ),
+          keyboardType: TextInputType.name, // le type peut êtrre choisit tel que : email, nombre, mdp etc...
+          obscureText: false, // pour masquer du texte
+          maxLength: 10, // longueur max de caractère
+          maxLines: 1, // nombre max de ligne
+          
+        ),
+        SizedBox(
+          height: 0, // espace entre les deux Textfield
+        ),
+         TextField(
+          decoration: InputDecoration(
+            hintText: "Entrer votre Mot de Passe", // hintext = placeholder
+            labelText: "Mot de Passe",  // Nom de  l'input (juste au dessus)
+            labelStyle: TextStyle( // labelStyle correspond à la balise <style> en css
+            fontSize: 24, color: Colors.blue), 
+            //border: OutlineInputBorder() // bordure autout d'un input
+            //border: InputBorder.none
+              suffixIcon: IconButton(
+                icon: Icon(Icons.security),
+                onPressed: (){
+                  setState(() {
+                    _secureText = !_secureText;
+                  });
+                },
+          ),
+                //suffixIcon pour mettre l'icone à droite
+            border: UnderlineInputBorder(),
+            fillColor: Colors.black12,
+            filled: true, // syntaxe obligatoire, par defaut le remplissage filled = false
+            obscureText: _secureText
+          ),
+          keyboardType: TextInputType.name, // le type peut êtrre choisit tel que : email, nombre, mdp etc...
+          obscureText: true, // pour masquer du texte
+          maxLength: 10, // longueur max de caractère
+          maxLines: 1, // nombre max de ligne
+          
+        ),
+            ],
       )),
     );
   }
