89949d9883b45743738d7c5a9214db48836aaa52
Baptiste-u <Baptistedelorme10@gmail.com>
2022-03-17 17:14:09 +0100

    no message

--- Tuto_2 (D)/lib/screens/inscription.dart
+++ Tuto_2 (D)/lib/screens/inscription.dart
@@ -1,144 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:gsb_application/screens/home.dart';
 
 class Inscription extends StatelessWidget {
   const Inscription({Key? key}) : super(key: key);
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-body: SingleChildScrollView(
-
-  child: Center( // Tout le container dans cette parenthèse va être centré
-  
-  
-  child: Container(
-
-    //alignment: Alignment.centerRight,
-    //margin: EdgeInsets.all(100.0),
-    decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.purpleAccent,
-          
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style =
+        TextButton.styleFrom(primary: Theme.of(context).colorScheme.onPrimary);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Projet GSB'),
+        actions: [
+          TextButton(
+            style: style,
+            onPressed: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Home()),
+              )
+            },
+            child: const Text('Page Connexion'),
           ),
-    padding: EdgeInsets.symmetric(vertical: 20.0),
-    width: 360,
-    
-    height: 600, //Padding
-      
-      child : Form(
-        
-        child: Column (
-          
-          
-            children: <Widget>[
-                //Align(
-                //alignment: Alignment.centerLeft),
-                
-                //Text('Test'),
-                
-                Image.asset('assets/images/gsb.png', height: 100.0, width: 100.0), // Propriété du logo
-                SizedBox(height: 25.0),// Définition des proportions de la 'box'
-                TextFormField(                
-                textAlign: TextAlign.left, // Ici c'est le text à l'intérieur de l'input qui est définit
-                decoration: InputDecoration(               
-                  labelText: 'Identifiant', // Nom de l'input
-                  border: UnderlineInputBorder(),            
-                  labelStyle : TextStyle( 
-                     
-                    color: Colors.black,
-                    fontSize: 17,
-                    fontWeight: FontWeight.w900,
-                    
-                      )
-                          
-                )
-                
-              ),
-                
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Mot de Passe',
-                    
-                    border: UnderlineInputBorder(),            
-                  labelStyle : TextStyle(  
-                    color: Colors.black,
-                    fontSize: 17,
-                    fontWeight: FontWeight.w900,
-                    
-                      )
-                  ),
-                  obscureText: true,
-                  
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Nom',
-                    border: UnderlineInputBorder(),            
-                  labelStyle : TextStyle(  
-                    color: Colors.black,
-                    fontSize: 17,
-                    fontWeight: FontWeight.w900,
-                    
-                      )
-                  )
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Prénom',
-                    border: UnderlineInputBorder(),            
-                  labelStyle : TextStyle(  
-                    color: Colors.black,
-                    fontSize: 17,
-                    fontWeight: FontWeight.w900,
-                    
-                      )
-                  )
-                ),
-                
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Mail',
-                    border: UnderlineInputBorder(),            
-                  labelStyle : TextStyle(  
-                    color: Colors.black,
-                    fontSize: 17,
-                    fontWeight: FontWeight.w900,
-                    
-                      )
-                  )
-                ),
-                Padding(
-
-            padding: const EdgeInsets.symmetric(vertical: 60.0),
-            child: ElevatedButton(
-              onPressed: () {
-                // Validate will return true if the form is valid, or false if
-                // the form is invalid.
-                
-              },
-              child: const Text('Envoyer'),
-                  
-              style: ElevatedButton.styleFrom(
-                //shadowColor: Colors.white,
-                primary: Colors.white,
-                onPrimary: Colors.purple, // background color
-                minimumSize: Size(340, 50),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30)
-                )
-              ),
-            ),
-          ),
-          
-
-              ]
-              
-            )
-          ),
-        ),
-  )
+          TextButton(
+            style: style,
+            onPressed: () {},
+            child: const Text('Historique'),
+          )
+        ],
       ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding:
+              EdgeInsets.symmetric(vertical: 50.0, horizontal: 30.0), //Padding
+          child: Form(
+              child: Column(children: <Widget>[
+            Image.asset('assets/images/gsb.png',
+                height: 100.0, width: 100.0), // Propriété du logo
+            SizedBox(height: 15.0), // Définition des proportions de la 'box'
+            TextFormField(
+                decoration: InputDecoration(
+                    labelText: 'Identifiant',
+                    // Nom de l'input
+                    border: OutlineInputBorder())),
 
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Mot de Passe',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            TextFormField(
+                decoration: InputDecoration(
+                    labelText: 'Nom', border: OutlineInputBorder())),
+            TextFormField(
+                decoration: InputDecoration(
+                    labelText: 'Prénom', border: OutlineInputBorder())),
+
+            TextFormField(
+                decoration: InputDecoration(
+                    labelText: 'Mail', border: OutlineInputBorder())),
+          ])),
+        ),
+      ),
     );
-    }
   }
+}
